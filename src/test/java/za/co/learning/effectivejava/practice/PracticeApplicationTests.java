4e337d29f5c2b972d6b5b57be29966d70d4544b7
James Ritala <jamesritala@Jamess-Air.Dlink>
2020-06-15 15:29:30 +0200

    Project creation. Springboot and Mavenised

---
+++ src/test/java/za/co/learning/effectivejava/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.learning.effectivejava.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
